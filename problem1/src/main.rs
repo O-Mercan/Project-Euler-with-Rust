dddb58eb7c9f57aa3da76732a14508ba9dfb2245
Ömer Mercan <70708913+O-Mercan@users.noreply.github.com>
2024-06-29 23:48:36 +0300

    Problem 1 added

--- problem1/src/main.rs
+++ problem1/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let mut sum = 0;
+    let limit = 1000;
+
+    for num in 1..limit {
+        if num % 3 == 0 || num % 5 == 0 {
+            sum += num;
+        }
+    }
+
+    println!("Sum of multiples of 3 or 5 below {}: {}", limit, sum);
 }
+
+//If we list all the natural numbers below 10 that are multiples of 3 or 5 we get 3,5,6, and 9.
+// The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
